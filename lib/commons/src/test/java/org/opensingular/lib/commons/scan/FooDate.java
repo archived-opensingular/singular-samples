a1d0c6502c853e38e42e5d36994b24dd8d69c15b
Vinicius Uriel <viniciusuriel@gmail.com>
2017-03-15 15:52:22 -0300

    movendo projetos, removendo libs do core

--- lib/commons/src/test/java/org/opensingular/lib/commons/scan/FooDate.java
+++
@@ -1,7 +0,0 @@
-package org.opensingular.lib.commons.scan;
-
-import java.util.Date;
-
-@Bar
-public class FooDate extends Date  {
-}
--- lib/commons/src/test/java/org/opensingular/lib/commons/scan/FooSerializable.java
+++
@@ -1,7 +0,0 @@
-package org.opensingular.lib.commons.scan;
-
-import java.io.Serializable;
-
-@Bar
-public class FooSerializable implements Serializable {
-}
