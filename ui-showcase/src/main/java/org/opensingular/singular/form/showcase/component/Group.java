3465ddb386bca3c6cba3c073aa365d33f56fdd20
torquato.neto <torquato.neto@opensingular.com>
2017-04-05 11:01:46 -0300

    Ajustes (favicon, contexto, componente Maps) e novos componentes.

--- ui-showcase/src/main/java/org/opensingular/singular/form/showcase/component/Group.java
+++ ui-showcase/src/main/java/org/opensingular/singular/form/showcase/component/Group.java
@@ -20,6 +20,7 @@ import org.opensingular.lib.wicket.util.resource.Icone;
 
 public enum Group {
     INPUT("Input", Icone.PUZZLE, ShowCaseType.FORM),
+    COUNTRY("Região", Icone.PUZZLE, ShowCaseType.FORM),
     FILE("File", Icone.FOLDER, ShowCaseType.FORM),
     LAYOUT("Layout", Icone.GRID, ShowCaseType.FORM),
     VALIDATION("Validation", Icone.BAN, ShowCaseType.FORM),
