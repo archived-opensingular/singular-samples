4f8bf080e61f5a1475f78e7fb2c61bdf750c1304
Vitor Mateus Costa do Rego <vitor.rego@miranteinfo.com>
2017-05-26 08:10:06 -0300

    Atualizando caseInputCorePackage

---
+++ ui-showcase/src/main/java/org/opensingular/singular/form/showcase/component/form/core/CaseInputCorePackage.java
@@ -0,0 +1,12 @@
+package org.opensingular.singular.form.showcase.component.form.core;
+
+import org.opensingular.form.PackageBuilder;
+import org.opensingular.form.SInfoPackage;
+import org.opensingular.form.SPackage;
+
+import javax.annotation.Nonnull;
+
+@SInfoPackage(name = CaseInputCorePackage.PACKAGE_NAME)
+public class CaseInputCorePackage extends SPackage {
+    public static final String PACKAGE_NAME = "org.opensingular.showcase";
+}
