9ad377bc9964db6ba1f0eaeffe4ccfcc334a374b
Danilo Mesquita <danilofmesquita@gmail.com>
2017-08-22 17:17:10 -0300

    Em construção -> Studio Base App

---
+++ studio-sample/src/main/java/org/opensingular/sample/studiop/form/FerramentaStudioDefinition.java
@@ -0,0 +1,23 @@
+package org.opensingular.sample.studiop.form;
+
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.opensingular.form.SIComposite;
+import org.opensingular.lib.wicket.util.datatable.BSDataTableBuilder;
+import org.opensingular.studio.app.definition.StudioDefinition;
+
+public class FerramentaStudioDefinition implements StudioDefinition {
+    @Override
+    public String getRepositoryBeanName() {
+        return "ferramentaRepository";
+    }
+
+    @Override
+    public void configureDatatableColumns(BSDataTableBuilder<SIComposite, String, IColumn<SIComposite, String>> dataTableBuilder) {
+        dataTableBuilder.appendPropertyColumn("Nome", it -> it.getValue("nome"));
+    }
+
+    @Override
+    public String getTitle() {
+        return "Cadastro de Ferramentas";
+    }
+}
--- studio-sample/src/main/java/org/opensingular/samples/studio/cfg/StudioSampleAppConfig.java
+++ studio-sample/src/main/java/org/opensingular/samples/studio/cfg/StudioSampleAppConfig.java
@@ -1,36 +1,26 @@
 package org.opensingular.samples.studio.cfg;
 
-import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
-import org.opensingular.form.SIComposite;
-import org.opensingular.lib.wicket.util.datatable.BSDataTableBuilder;
 import org.opensingular.lib.wicket.util.resource.DefaultIcons;
+import org.opensingular.sample.studiop.form.FerramentaStudioDefinition;
+import org.opensingular.sample.studiop.form.PresenteStudioDefinition;
 import org.opensingular.studio.app.AbstractStudioAppConfig;
 import org.opensingular.studio.app.menu.StudioMenuItem;
 import org.opensingular.studio.core.menu.GroupMenuEntry;
 import org.opensingular.studio.core.menu.StudioMenu;
 
 public class StudioSampleAppConfig extends AbstractStudioAppConfig {
+
     @Override
     public StudioMenu getAppMenu() {
         StudioMenu menu = new StudioMenu();
         GroupMenuEntry departamento = menu.add(new GroupMenuEntry(DefaultIcons.BRIEFCASE, "Departamento"));
-        departamento.add(new StudioMenuItem(DefaultIcons.GIFT, "Presentes", "presenteRepository") {
-            @Override
-            public void configureTable(BSDataTableBuilder<SIComposite, String, IColumn<SIComposite, String>> dataTableBuilder) {
-                dataTableBuilder.appendPropertyColumn("Nome", it -> it.getValue("nome"));
-            }
-        });
-        departamento.add(new StudioMenuItem(DefaultIcons.WRENCH, "Ferramentas", "ferramentaRepository") {
-            @Override
-            public void configureTable(BSDataTableBuilder<SIComposite, String, IColumn<SIComposite, String>> dataTableBuilder) {
-                dataTableBuilder.appendPropertyColumn("Nome", it -> it.getValue("nome"));
-            }
-        });
+        departamento.add(new StudioMenuItem(DefaultIcons.GIFT, "Presentes", new PresenteStudioDefinition()));
+        departamento.add(new StudioMenuItem(DefaultIcons.WRENCH, "Ferramentas", new FerramentaStudioDefinition()));
         return menu;
     }
 
     @Override
     public Class<?>[] getSpringAnnotatedConfigs() {
-       return new Class<?>[]{StudioSampleBeanFactory.class};
+        return new Class<?>[]{StudioSampleBeanFactory.class};
     }
 }
