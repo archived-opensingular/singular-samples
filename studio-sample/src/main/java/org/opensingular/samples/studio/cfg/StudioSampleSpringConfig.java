2b8ac9d8ed40337ea801a5c9cc8ac70017de0732
Danilo Mesquita <danilofmesquita@gmail.com>
2017-08-25 20:04:42 -0300

    Melhorando declaração de beans

--- studio-sample/src/main/java/org/opensingular/samples/studio/cfg/StudioSampleSpringConfig.java
+++ studio-sample/src/main/java/org/opensingular/samples/studio/cfg/StudioSampleSpringConfig.java
@@ -1,42 +1,25 @@
 package org.opensingular.samples.studio.cfg;
 
-import org.opensingular.form.SFormUtil;
 import org.opensingular.form.SIComposite;
-import org.opensingular.form.document.SDocumentFactory;
 import org.opensingular.form.spring.SpringFormPersistenceInMemory;
-import org.opensingular.form.spring.SpringSDocumentFactoryEmpty;
-import org.opensingular.form.spring.SpringServiceRegistry;
-import org.opensingular.form.wicket.SingularFormConfigWicketImpl;
-import org.opensingular.lib.context.singleton.SpringBoundedSingletonStrategy;
 import org.opensingular.sample.studiop.form.STFerramenta;
 import org.opensingular.sample.studiop.form.STPresente;
-import org.opensingular.sample.studiop.form.StudioPackage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Collections;
 
 @Configuration
 @ComponentScan(value = {"org.opensingular.samples.studio", "org.opensingular.lib.support.spring.util"})
 public class StudioSampleSpringConfig {
 
     @Bean(name = "presenteRepository")
-    public SpringFormPersistenceInMemory<STPresente, SIComposite> presenteRepository(SDocumentFactory documentFactory) {
-        SpringFormPersistenceInMemory<STPresente, SIComposite> s = new SpringFormPersistenceInMemory<>();
-        s.setTypeFullName(SFormUtil.getTypeName(STPresente.class));
-        s.setDocumentFactory(documentFactory);
-        s.setPackageClasses(Collections.singletonList(StudioPackage.class));
-        return s;
+    public SpringFormPersistenceInMemory<STPresente, SIComposite> presenteRepository() {
+        return new SpringFormPersistenceInMemory<>(STPresente.class);
     }
 
     @Bean(name = "ferramentaRepository")
-    public SpringFormPersistenceInMemory<STFerramenta, SIComposite> ferramentaRepository(SDocumentFactory documentFactory) {
-        SpringFormPersistenceInMemory<STFerramenta, SIComposite> s = new SpringFormPersistenceInMemory<>();
-        s.setTypeFullName(SFormUtil.getTypeName(STFerramenta.class));
-        s.setDocumentFactory(documentFactory);
-        s.setPackageClasses(Collections.singletonList(StudioPackage.class));
-        return s;
+    public SpringFormPersistenceInMemory<STFerramenta, SIComposite> ferramentaRepository() {
+        return new SpringFormPersistenceInMemory<>(STFerramenta.class);
     }
 
 }
