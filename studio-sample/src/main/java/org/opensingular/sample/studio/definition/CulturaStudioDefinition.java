0a2d394e6600e73b04e9d2921ec9e46e8d7c5c5d
Danilo Mesquita <danilofmesquita@gmail.com>
2017-08-30 15:48:33 -0300

    adicionando controle de permissões ao studio definition

--- studio-sample/src/main/java/org/opensingular/sample/studio/definition/CulturaStudioDefinition.java
+++ studio-sample/src/main/java/org/opensingular/sample/studio/definition/CulturaStudioDefinition.java
@@ -2,6 +2,7 @@ package org.opensingular.sample.studio.definition;
 
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.opensingular.form.SIComposite;
+import org.opensingular.form.studio.StudioCRUDPermissionStrategy;
 import org.opensingular.lib.wicket.util.datatable.BSDataTableBuilder;
 import org.opensingular.studio.app.definition.StudioDefinition;
 
@@ -19,5 +20,10 @@ public class CulturaStudioDefinition implements StudioDefinition {
     @Override
     public String getTitle() {
         return "Cadastro de Culturas";
+    }
+
+    @Override
+    public StudioCRUDPermissionStrategy getPermissionStrategy() {
+        return StudioCRUDPermissionStrategy.VIEW_ONLY;
     }
 }
