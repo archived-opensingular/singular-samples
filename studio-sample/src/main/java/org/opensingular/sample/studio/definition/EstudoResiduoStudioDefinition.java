76eeb1333539eca2b397cb6e3eddd78944ee3840
Danilo Mesquita <danilofmesquita@gmail.com>
2017-09-12 13:21:02 -0300

    reorganizando classes do studio (movendo maximo possivel para o core)

--- studio-sample/src/main/java/org/opensingular/sample/studio/definition/EstudoResiduoStudioDefinition.java
+++ studio-sample/src/main/java/org/opensingular/sample/studio/definition/EstudoResiduoStudioDefinition.java
@@ -4,7 +4,7 @@ import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.opensingular.form.SIComposite;
 import org.opensingular.lib.wicket.util.datatable.BSDataTableBuilder;
 import org.opensingular.sample.studio.form.EstudoResiduo;
-import org.opensingular.studio.app.definition.StudioDefinition;
+import org.opensingular.studio.core.definition.StudioDefinition;
 
 public class EstudoResiduoStudioDefinition implements StudioDefinition {
     @Override
