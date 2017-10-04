bf0d41a4f2987ee100e878d2f3f1947a2a09543d
Danilo Mesquita <danilofmesquita@gmail.com>
2017-10-04 17:40:57 -0300

    refatorando studio para permitir incluir paineis wickets aleatorios

---
+++ studio-sample/src/main/java/org/opensingular/sample/studio/wicket/WicketStudioContent.java
@@ -0,0 +1,11 @@
+package org.opensingular.sample.studio.wicket;
+
+import org.opensingular.studio.core.menu.MenuEntry;
+import org.opensingular.studio.core.view.StudioContent;
+
+public class WicketStudioContent extends StudioContent {
+
+    public WicketStudioContent(String id, MenuEntry currentMenuEntry) {
+        super(id, currentMenuEntry);
+    }
+}
