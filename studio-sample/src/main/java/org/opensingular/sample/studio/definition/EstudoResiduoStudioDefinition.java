86d4a16fb28b37fe0e2647373daaefb2411fadba
Danilo Mesquita <danilofmesquita@gmail.com>
2017-10-06 14:45:58 -0300

    adicionando possibilidade de modificar os conteudos de edição e listagem

--- studio-sample/src/main/java/org/opensingular/sample/studio/definition/EstudoResiduoStudioDefinition.java
+++ studio-sample/src/main/java/org/opensingular/sample/studio/definition/EstudoResiduoStudioDefinition.java
@@ -1,8 +1,16 @@
 package org.opensingular.sample.studio.definition;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.IModel;
+import org.opensingular.form.SInstance;
 import org.opensingular.sample.studio.repository.EstudoResiduoRepository;
 import org.opensingular.studio.core.definition.StudioDefinition;
 import org.opensingular.studio.core.definition.StudioTableDefinition;
+import org.opensingular.studio.core.panel.CrudEditContent;
+import org.opensingular.studio.core.panel.CrudShellContent;
+import org.opensingular.studio.core.panel.CrudShellManager;
 
 public class EstudoResiduoStudioDefinition implements StudioDefinition {
 
@@ -24,4 +32,25 @@ public class EstudoResiduoStudioDefinition implements StudioDefinition {
     public String getTitle() {
         return "Cadastro de Estudos de Residuos";
     }
+
+
+    @Override
+    public CrudEditContent makeEditContent(CrudShellManager crudShellManager, CrudShellContent previousContent, IModel<SInstance> instance) {
+        CrudEditContent crudEditContent = new CrudEditContent(crudShellManager, previousContent, instance);
+        crudEditContent.setSaveButtonFactory(new CrudEditContent.SaveButtonFactory(crudShellManager) {
+            @Override
+            public Button make(String id, IModel<SInstance> instanceModel) {
+                return new CrudEditContent.StudioSaveButton(id, instanceModel, crudShellManager) {
+                    @Override
+                    protected void onValidationSuccess(AjaxRequestTarget target, Form<?> form, IModel<? extends SInstance> instanceModel) {
+                        crudShellManager.addConfirm("Tem certeza que deseja salvar?", target, newTarget -> {
+                            super.onValidationSuccess(newTarget, form, instanceModel);
+                        });
+                    }
+                };
+            }
+        });
+        return crudEditContent;
+    }
+
 }
