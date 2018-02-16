52279b916b5df16a1b2334ac86d1b8853d56fa34
Douglas Silva <douglas.silva@mirante.net.br>
2018-02-16 12:56:47 -0200

    [SGL-558] Refatorando módulos enterprise.

--- enterprise-ws-sample/src/main/java/com/opensingular/ws/requirement/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "com.opensingular")
-package com.opensingular.ws.requirement;
--- enterprise-ws-sample/src/main/java/com/opensingular/ws/upload/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "opensingular.com")
-package com.opensingular.ws.upload;
