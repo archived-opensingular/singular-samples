8a4e4626734a083e7b6ae70ffdafdd87d61f0068
thiago.bomfim <thiago.bomfim@opensingular.com>
2018-07-25 08:20:06 -0300

    [SGL-613] - Confiuração do SingularQuartzJob que faz integração com o Spring para permitir a utilização do dataSource

--- requirement-sample/requirement-sample-app/src/main/java/org/opensingular/requirementsamplemodule/config/SamplePersistenceConfiguration.java
+++ requirement-sample/requirement-sample-app/src/main/java/org/opensingular/requirementsamplemodule/config/SamplePersistenceConfiguration.java
@@ -21,7 +21,7 @@ package org.opensingular.requirementsamplemodule.config;
 import java.util.List;
 
 import org.hibernate.dialect.Dialect;
-import org.hibernate.dialect.H2Dialect;
+import org.hibernate.dialect.SQLServerDialect;
 import org.opensingular.app.commons.spring.persistence.database.PackageScanConfiguration;
 import org.opensingular.app.commons.spring.persistence.database.SingularPersistenceConfiguration;
 
@@ -40,7 +40,7 @@ public class SamplePersistenceConfiguration implements SingularPersistenceConfig
 
     @Override
     public Class<? extends Dialect> getHibernateDialect() {
-        return H2Dialect.class;
+        return SQLServerDialect.class;
     }
 
 
