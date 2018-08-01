0c5f64bfa2f6758ef99000810f3c21f16e17665f
Daniel Bordin <daniel.bordin@opensingular.com>
2018-08-01 00:20:17 -0300

    [SGL-768] Melhorando o processador de fontes Java para automatizar a remoção de imports desnecessário e as Anotattions específicas do showcase

--- ui-showcase/src/main/java/org/opensingular/singular/form/showcase/view/page/SourceCodeProcessor.java
+++ ui-showcase/src/main/java/org/opensingular/singular/form/showcase/view/page/SourceCodeProcessor.java
@@ -16,41 +16,82 @@
 
 package org.opensingular.singular.form.showcase.view.page;
 
+import org.apache.commons.lang3.StringUtils;
+import org.opensingular.singular.form.showcase.component.CaseItem;
+import org.opensingular.singular.form.showcase.component.Group;
+import org.opensingular.singular.form.showcase.component.Resource;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 class SourceCodeProcessor {
 
     private final String sourceCode;
-    private final List<Integer> linesToBeHighlighted;
-    private final List<String> finalSourceCode;
-    private final List<String> classJavadoc;
-    private final static List<String> TO_BE_IGNORED = Collections.singletonList("@formatter");
+    private final List<String> classJavadoc = new ArrayList<>();
+    private final List<Predicate<String>> lineToBeIgnoredConditions = new ArrayList<>();
+    private final List<Line> source = new ArrayList<>();
+
+    private boolean highLightBlockOn = false;
+    private boolean highLightNextLine = false;
 
     public SourceCodeProcessor(String sourceCode) {
         this.sourceCode = sourceCode;
-        this.linesToBeHighlighted = new ArrayList<>();
-        this.finalSourceCode = new ArrayList<>();
-        this.classJavadoc = new ArrayList<>();
-        process();
+        loadIgnoreLines();
+        splitSourceAndJavadoc();
+        removeLinesBeforePackage();
+        removeHiddenBlocks();
+        removeIgnoreLines();
+        removeCaseItemAnnotation();
     }
 
-    private void process() {
-        boolean beforePackage = true;
+    private void removeCaseItemAnnotation() {
+        String target = "@" + CaseItem.class.getSimpleName();
+        for (int linIni = 0; linIni < source.size(); linIni++) {
+            int pos = source.get(linIni).getLine().indexOf(target);
+            if (pos != -1) {
+                int linEnd = findLastLineClosingAnnotation(linIni);
+                removeSourceLines(linIni, linEnd + 1);
+                return;
+            }
+        }
+    }
+
+    private int findLastLineClosingAnnotation(int linIni) {
+        int parenthesis = 0;
+        for(int linEnd = linIni; linEnd < source.size(); linEnd++) {
+            parenthesis = countParenthesis(linEnd, parenthesis);
+            if (parenthesis == 0) {
+                return linEnd;
+            }
+        }
+        return source.size() - 1;
+    }
+
+    private int countParenthesis(int lin, int parenthesis) {
+        int count = parenthesis;
+        String line = source.get(lin).getLine();
+        for (int p = 0; p < line.length(); p++) {
+            if (line.charAt(p) == ')') {
+                count--;
+                if (count == 0) {
+                    return 0;
+                }
+            } else if (line.charAt(p) == '(') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private void splitSourceAndJavadoc() {
         boolean javadoc = false;
         boolean classStarted = false;
         final String[] lines = sourceCode.split("\n");
 
         for (int i = 0; i < lines.length; i += 1) {
             final String line = lines[i];
-            if (beforePackage) {
-                if (line.startsWith("package")) {
-                    beforePackage = false;
-                } else {
-                    continue;
-                }
-            }
             if (javadoc) {
                 javadoc = processJavaDoc(line);
                 continue;
@@ -59,39 +100,90 @@ class SourceCodeProcessor {
                     javadoc = true;
                     continue;
                 }
-                classStarted = line.contains("public class ");
+                classStarted = isClassBegin(line);
             }
-            if (!isLineToBeIgnored(line)) {
-                i = analyzeSourceLines(lines, i, line);
-            }
+            analyzeSourceLines(lines, i, line);
         }
     }
 
-    private int analyzeSourceLines(String[] lines, int i, String line) {
-        int j = i;
-        if (isBeShow(line)) {
-            if (isBlock(line)) {
-                j = processBlock(lines, j);
-            } else if (isLine(line)) {
-                finalSourceCode.add(lines[++j]);
-                linesToBeHighlighted.add(finalSourceCode.size());
+    private boolean isClassBegin(String line) {
+        return line.contains("public class ");
+    }
+
+    private void removeLinesBeforePackage() {
+        int packageLine = findLine(0, line -> line.isStartWith("package"));
+        if (packageLine > 0) {
+            removeSourceLines(0, packageLine);
+        }
+    }
+
+    private void removeHiddenBlocks() {
+        int i = 0;
+        while (i < source.size()) {
+            int pos = findLine(i, line -> line.isStartWith("//hidden:begin"));
+            if (pos != -1) {
+                int posF = findLine(pos + 1, line -> line.isStartWith("//hidden:end"));
+                if (posF == -1) {
+                    removeSourceLines(pos, source.size());
+                } else {
+                    removeSourceLines(pos, posF + 1);
+                }
+                removeDoubleBlankLine(pos);
+                i = pos;
             } else {
-                finalSourceCode.add(line);
+                i++;
             }
         }
-        return j;
     }
 
-    private boolean isBeShow(String line) {
-        return !line.startsWith("/*hidden*/") && !line.contains("TODO");
-    }
-
-    private int processBlock(String[] lines, int i) {
-        while (!isEndOfBlock(lines[++i])) {
-            finalSourceCode.add(lines[i]);
-            linesToBeHighlighted.add(finalSourceCode.size());
+    private void removeIgnoreLines() {
+        int i = 0;
+        while (i < source.size()) {
+            if (isLineToBeIgnored(source.get(i).getLine())) {
+                removeSourceLines(i, i + 1);
+                removeDoubleBlankLine(i);
+            } else {
+                i++;
+            }
         }
-        return i;
+    }
+
+    private void removeDoubleBlankLine(int pos) {
+        if (pos > 0 && isBlankLine(pos) && isBlankLine(pos - 1)) {
+            removeSourceLines(pos, pos + 1);
+        }
+    }
+
+    private boolean isBlankLine(int pos) {
+        return pos >= 0 && StringUtils.isBlank(source.get(pos).getLine());
+    }
+
+    private int findLine(int begin, Predicate<Line> condition) {
+        for (int i = begin; i < source.size(); i++) {
+            if (condition.test(source.get(i))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void analyzeSourceLines(String[] lines, int i, String line) {
+        if (isHighlightBlockBegin(line)) {
+            highLightBlockOn = true;
+        } else if (highLightBlockOn && isHighlightBlockEnd(line)) {
+            highLightNextLine = false;
+            highLightBlockOn = false;
+        } else {
+            int pos = getHighlightStartComment(line);
+            if (pos == -1) {
+                source.add(new Line(lines[i], highLightBlockOn || highLightNextLine));
+                highLightNextLine = false;
+            } else if (pos == 0 || StringUtils.isBlank(line.substring(0, pos))) {
+                highLightNextLine = true;
+            } else {
+                source.add(new Line(line.substring(0, pos), true));
+            }
+        }
     }
 
     private boolean processJavaDoc(String line) {
@@ -111,31 +203,24 @@ class SourceCodeProcessor {
         return true;
     }
 
-    private boolean isBlock(String candidate) {
+    private boolean isHighlightBlockBegin(String candidate) {
         return candidate.contains("//@destacar:bloco") || candidate.contains("// @destacar:bloco");
     }
 
-    private boolean isEndOfBlock(String candidate) {
+    private boolean isHighlightBlockEnd(String candidate) {
         return candidate.contains("//@destacar:fim") || candidate.contains("// @destacar:fim");
     }
 
-    private boolean isLine(String candidate) {
-        return candidate.contains("//@destacar") || candidate.contains("// @destacar");
-    }
-
-    private boolean isLineToBeIgnored(String candidate) {
-        for (String ignore : TO_BE_IGNORED) {
-            if (candidate.contains(ignore)) {
-                return true;
-            }
+    private int getHighlightStartComment(String line) {
+        int pos = line.indexOf("//@destacar");
+        if (pos == -1) {
+            pos = line.indexOf("// @destacar");
         }
-        return false;
+        return pos;
     }
 
     public String getResultSourceCode() {
-        StringBuilder sb = new StringBuilder();
-        finalSourceCode.forEach(s -> sb.append(s).append('\n'));
-        return sb.toString();
+        return source.stream().map(Line::getLine).collect(Collectors.joining("\n"));
     }
 
     public String getJavadoc() {
@@ -149,6 +234,69 @@ class SourceCodeProcessor {
     }
 
     public List<Integer> getLinesToBeHighlighted() {
-        return linesToBeHighlighted;
+        List<Integer> highlight = new ArrayList<>();
+        for (int i = 0; i < source.size(); i++) {
+            if (source.get(i).isHighlighted()) {
+                highlight.add(i + 1);
+            }
+        }
+        return highlight;
+    }
+
+    private void removeSourceLines(int begin, int endExcluding) {
+        for (int i = endExcluding - 1; i >= begin; i--) {
+            source.remove(i);
+        }
+    }
+
+    private boolean isLineToBeIgnored(String candidate) {
+        return lineToBeIgnoredConditions.stream().anyMatch(condition -> condition.test(candidate));
+    }
+
+    private void loadIgnoreLines() {
+        addIgnoreImport(CaseItem.class);
+        addIgnoreImport(Group.class);
+        addIgnoreImport(Resource.class);
+        addIgnoreLineContaining("@formatter");
+        addIgnoreLineContaining("TODO");
+        addIgnoreLineContaining("/*hidden*/");
+    }
+
+    private void addIgnoreImport(Class<?> c) {
+        addIgnoreLineStartingWith("import " + c.getName());
+    }
+
+    private void addIgnoreLineStartingWith(String s) {
+        lineToBeIgnoredConditions.add(line -> line.trim().startsWith(s));
+    }
+
+    private void addIgnoreLineContaining(String s) {
+        lineToBeIgnoredConditions.add(line -> line.contains(s));
+    }
+
+    private static class Line {
+        private final String line;
+        private boolean highlighted;
+
+        private Line(String line, boolean highlighted) {
+            this.line = line;
+            this.highlighted = highlighted;
+        }
+
+        public String getLine() {
+            return line;
+        }
+
+        public boolean isStartWith(String s) {
+            return line.trim().startsWith(s);
+        }
+
+        public boolean isHighlighted() {
+            return highlighted;
+        }
+
+        public void setHighlighted(boolean highlighted) {
+            this.highlighted = highlighted;
+        }
     }
 }
--- ui-showcase/src/test/java/org/opensingular/singular/form/showcase/view/page/SourceCodeProcessorTest.java
+++ ui-showcase/src/test/java/org/opensingular/singular/form/showcase/view/page/SourceCodeProcessorTest.java
@@ -84,10 +84,127 @@ public class SourceCodeProcessorTest {
     public void testProcessadorCodigoFonte(){
         SourceCodeProcessor sourceCodeProcessor = new SourceCodeProcessor(mockCodigoFonte());
 
-        sourceCodeProcessor.getResultSourceCode();
-
         assertThat(sourceCodeProcessor.getLinesToBeHighlighted()).hasSize(8);
         assertThat(sourceCodeProcessor.getJavadoc()).isEqualTo(" Linha 1\n Linha 2\nend javadoc\n");
         assertThat(sourceCodeProcessor.getResultSourceCode()).isNotNull().startsWith("package ");
+    }
+
+    @Test
+    public void testRemoveInternalImports() {
+        StringBuilder code = new StringBuilder();
+        code.append("package org.example;\n");
+        code.append("\n");
+        code.append("import org.opensingular.form.SIComposite;\n");
+        code.append("import org.opensingular.singular.form.showcase.component.CaseItem;\n");
+        code.append("import org.opensingular.singular.form.showcase.component.Group;\n");
+        code.append("import org.opensingular.singular.form.showcase.component.Resource;\n");
+        code.append("import org.opensingular.form.SInfoType;\n");
+        code.append("\n");
+        code.append("public class Test {}");
+
+        StringBuilder exp = new StringBuilder();
+        exp.append("package org.example;\n");
+        exp.append("\n");
+        exp.append("import org.opensingular.form.SIComposite;\n");
+        exp.append("import org.opensingular.form.SInfoType;\n");
+        exp.append("\n");
+        exp.append("public class Test {}");
+
+        SourceCodeProcessor processor = new SourceCodeProcessor(code.toString());
+        assertThat(processor.getResultSourceCode()).isEqualTo(exp.toString());
+    }
+
+    @Test
+    public void testRemoveHiddenBlock() {
+        StringBuilder code = new StringBuilder();
+        code.append("package org.example;\n");
+        code.append("\n");
+        code.append("//hidden:begin\n");
+        code.append("import org.opensingular.form.SIComposite;\n");
+        code.append("//hidden:end\n");
+        code.append("\n");
+        code.append("public class Test {}");
+
+        StringBuilder exp = new StringBuilder();
+        exp.append("package org.example;\n");
+        exp.append("\n");
+        exp.append("public class Test {}");
+
+        SourceCodeProcessor processor = new SourceCodeProcessor(code.toString());
+        assertThat(processor.getResultSourceCode()).isEqualTo(exp.toString());
+    }
+
+    @Test
+    public void testRemoveHiddenComment() {
+        StringBuilder code = new StringBuilder();
+        code.append("package org.example;\n");
+        code.append("\n");
+        code.append("/*hidden*/\n");
+        code.append(" /*hidden*/\n");
+        code.append("/*hidden*/A\n");
+        code.append(" /*hidden*/B\n");
+        code.append(" C/*hidden*/\n");
+        code.append("D /*hidden*/\n");
+        code.append("E/*hidden*/\n");
+        code.append("\n");
+        code.append("public class Test {}");
+
+        StringBuilder exp = new StringBuilder();
+        exp.append("package org.example;\n");
+        exp.append("\n");
+        exp.append("public class Test {}");
+
+        SourceCodeProcessor processor = new SourceCodeProcessor(code.toString());
+        assertThat(processor.getResultSourceCode()).isEqualTo(exp.toString());
+    }
+
+    @Test
+    public void testRemoveCaseAnnotationSimple() {
+        StringBuilder code = new StringBuilder();
+        code.append("package org.example;\n");
+        code.append("\n");
+        code.append("@CaseItem()\n");
+        code.append("public class Test {}");
+
+        StringBuilder exp = new StringBuilder();
+        exp.append("package org.example;\n");
+        exp.append("\n");
+        exp.append("public class Test {}");
+
+        SourceCodeProcessor processor = new SourceCodeProcessor(code.toString());
+        assertThat(processor.getResultSourceCode()).isEqualTo(exp.toString());
+    }
+
+    @Test
+    public void testRemoveCaseAnnotationMultiLine() {
+        StringBuilder code = new StringBuilder();
+        code.append("package org.example;\n");
+        code.append("\n");
+        code.append("@CaseItem(\n");
+        code.append(")\n");
+        code.append("public class Test {}");
+
+        StringBuilder exp = new StringBuilder();
+        exp.append("package org.example;\n");
+        exp.append("\n");
+        exp.append("public class Test {}");
+
+        assertThat(new SourceCodeProcessor(code.toString()).getResultSourceCode()).isEqualTo(exp.toString());
+    }
+
+    @Test
+    public void testRemoveCaseAnnotationMultiParathesis() {
+        StringBuilder code = new StringBuilder();
+        code.append("package org.example;\n");
+        code.append("\n");
+        code.append("@CaseItem(componentName = \"Basic\", resources = @Resource(CaseInputCorePackage.class))\n");
+        code.append("public class Test {}");
+
+        StringBuilder exp = new StringBuilder();
+        exp.append("package org.example;\n");
+        exp.append("\n");
+        exp.append("public class Test {}");
+
+        assertThat(new SourceCodeProcessor(code.toString()).getResultSourceCode()).isEqualTo(exp.toString());
     }
 }
