2afabcc80a0b3dd2c1b65340ea8e659e8287c140
Danilo Mesquita <danilofmesquita@gmail.com>
2018-08-07 10:10:35 -0300

    [SGL-749] Melhorando legibilidade do script de QuickNav

--- requirement-sample/requirement-sample-app/src/main/java/org/sample/form/RequirementsampleForm.java
+++ requirement-sample/requirement-sample-app/src/main/java/org/sample/form/RequirementsampleForm.java
@@ -10,19 +10,23 @@ import org.opensingular.form.view.SViewTab;
 public class RequirementsampleForm extends STypeComposite<SIComposite> {
 
     public STypeDadosPessoais dadosPessoais;
-    public STypeDadosPessoais exemplo21;
+    public EngenheiroForm engenheiroForm;
 
     @Override
     protected void onLoadType(TypeBuilder tb) {
         this.asAtrIndex().indexed(Boolean.TRUE);
         dadosPessoais = addField("dadosPessoais", STypeDadosPessoais.class);
-        exemplo21 = addField("exemplo21", STypeDadosPessoais.class);
+        engenheiroForm = addField("engenheiroForm", EngenheiroForm.class);
+
         dadosPessoais.asAtrIndex().indexed(Boolean.TRUE);
+
         SViewTab sViewTab = new SViewTab();
-        sViewTab.addTab(dadosPessoais);
-        sViewTab.addTab(exemplo21);
-        this.withView(sViewTab);
-        this.asAtr().label("Formulário de Requerimento").displayString("Requerimento de ${dadosPessoais.nomeCompleto!}");
+        sViewTab.addTab(dadosPessoais, "Dados Pessoais");
+        sViewTab.addTab(engenheiroForm, "Engenheiro");
+        withView(sViewTab);
+
+        asAtr().label("Formulário de Requerimento")
+                .displayString("Requerimento de ${dadosPessoais.nomeCompleto!}");
     }
 
 }
