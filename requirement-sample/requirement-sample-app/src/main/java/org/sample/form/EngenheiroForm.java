7924617fd8c5817d04ef38343dab327a0d2907a0
thiago.bomfim <thiago.bomfim@opensingular.com>
2018-08-09 10:58:37 -0300

    [SGL-773] - Criação de uma lista by table para simular o problema do jira

--- requirement-sample/requirement-sample-app/src/main/java/org/sample/form/EngenheiroForm.java
+++ requirement-sample/requirement-sample-app/src/main/java/org/sample/form/EngenheiroForm.java
@@ -1,10 +1,14 @@
 package org.sample.form;
 
 import org.opensingular.form.SIComposite;
+import org.opensingular.form.SIList;
 import org.opensingular.form.SInfoType;
 import org.opensingular.form.STypeComposite;
+import org.opensingular.form.STypeList;
 import org.opensingular.form.TypeBuilder;
+import org.opensingular.form.type.core.SIString;
 import org.opensingular.form.type.core.STypeString;
+import org.opensingular.form.view.SViewListByTable;
 
 import javax.annotation.Nonnull;
 
@@ -12,9 +16,23 @@ import javax.annotation.Nonnull;
 public class EngenheiroForm extends STypeComposite<SIComposite> {
 
     public STypeString nome;
+    public STypeList<STypeString, SIString> listaNomes;
 
     @Override
     protected void onLoadType(@Nonnull TypeBuilder tb) {
         nome = addField("nome", STypeString.class);
+
+        listaNomes = this.addFieldListOf("listaNomes", STypeString.class);
+        listaNomes.asAtr().label("Nomes");
+        listaNomes.withView(SViewListByTable::new);
+        listaNomes.withInitListener(this::createOptions);
+    }
+
+
+    private void createOptions(SIList<SIString> list) {
+        for (int i = 0; i < 20; i++) {
+            SIString experiencia = list.addNew();
+            experiencia.setValue("Nome" + i);
+        }
     }
 }
