41733b087e202232c382de21714c3878f25094f9
vinicius.nunes <vinicius.nunes@opensingular.com>
2018-11-28 10:43:02 -0200

    [SGL-882] ADDING OPTION ON MASTER DETAIL TO ENFORCE VALIDATION DURING NEW ELEMENT CONFIRMATION (MODAL CONFIRMATION)

---
+++ ui-showcase/src/main/java/org/opensingular/singular/form/showcase/component/form/layout/masterdetail/CaseListByMasterDetailEnforceValidation.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2016 Singular Studios (a.k.a Atom Tecnologia) - www.opensingular.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensingular.singular.form.showcase.component.form.layout.masterdetail;
+
+import org.opensingular.form.SIComposite;
+import org.opensingular.form.SInfoType;
+import org.opensingular.form.STypeComposite;
+import org.opensingular.form.STypeList;
+import org.opensingular.form.TypeBuilder;
+import org.opensingular.form.view.SViewListByMasterDetail;
+import org.opensingular.singular.form.showcase.component.CaseItem;
+import org.opensingular.singular.form.showcase.component.Group;
+import org.opensingular.singular.form.showcase.component.Resource;
+import org.opensingular.singular.form.showcase.component.form.layout.CaseLayoutPackage;
+import org.opensingular.singular.form.showcase.component.form.layout.stypes.STypeExperienciaProfissional;
+
+import javax.annotation.Nonnull;
+
+/**
+ * List by Master Detail
+ */
+@CaseItem(componentName = "List by Master Detail", subCaseName = "Validar elemento antes de adicionar", group = Group.LAYOUT,
+        resources = {@Resource(STypeExperienciaProfissional.class), @Resource(CaseLayoutPackage.class)})
+@SInfoType(spackage = CaseLayoutPackage.class, name = "EnforceValidationMasterDetail")
+public class CaseListByMasterDetailEnforceValidation extends STypeComposite<SIComposite> {
+
+    public STypeList<STypeExperienciaProfissional, SIComposite> experienciasProfissionais;
+
+    @Override
+    protected void onLoadType(@Nonnull TypeBuilder tb) {
+        experienciasProfissionais = this.addFieldListOf("experienciasProfissionais", STypeExperienciaProfissional.class);
+
+
+        experienciasProfissionais
+                .getElementsType()
+                .inicio
+                .asAtr()
+                .required(true);
+
+        //@destacar:bloco
+        experienciasProfissionais.withView(new SViewListByMasterDetail()
+                .enforceValidationOnAdd())
+                //@destacar:fim
+                .asAtr().label("Experiências profissionais");
+    }
+}
